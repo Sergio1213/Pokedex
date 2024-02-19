23d994fd15f62013f73805548f6308e8ecadc9af
Sergio <sggr40501@hotmail.com>
2024-02-19 14:13:16 -0600

    final script

--- script.js
+++ script.js
@@ -2,7 +2,32 @@ const reqPokemon = () => {
   axios
     .get('https://pokeapi.co/api/v2/pokemon/pikachu')
     .then(function (response) {
+      const {
+        data: {
+          name,
+          height,
+          weight,
+          id,
+          types,
+          sprites: { front_default: image_pokemon },
+        },
+      } = response;
       console.log(response);
+      console.log(
+        'name: ',
+        name,
+        'height: ',
+        height,
+        ' weight: ',
+        weight,
+        ' id: ',
+        id,
+        'url_image_pokemon: ',
+        image_pokemon
+      );
+      for (let i = 0; i < types.length; i++) {
+        console.log(types[i].type.name);
+      }
     });
 };
 reqPokemon();
